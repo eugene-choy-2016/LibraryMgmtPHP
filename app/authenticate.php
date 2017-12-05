08990964779554f256228f6ef7fe001394135142
Eugene Choy <eugene.choy.2016@sis.smu.edu.sg>
2017-12-06 00:10:22 +0800

    Added config.php to store DB details Added Login.php with simple log in UI Added authenticate.php

---
+++ app/authenticate.php
@@ -0,0 +1,14 @@
+<?php
+    include('config.php');
+    session_start(); //Start session
+    
+    if(empty($_POST['username'])||empty($_POST['password'])){
+        
+    }
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ ?>
+
---
+++ app/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Login Page</title>
+    </head>
+    <body>
+        <h1>Library Management System</h1>
+        <form action="authenticate.php" method="post" name="login">
+            Username <input type="text" name="username"/><br/><br/>
+            Password <input type="password" name="password"/><br/><br/>
+            <input type="submit"/>
+            
+        </form>
+    </body>
+</html>
